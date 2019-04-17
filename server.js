9de23dfc4100d0b9bb853e125a0235978cad1239
Phạm Vũ Phương <phamvuphuong98@gmail.com>
2019-04-17 11:47:29 +0700

    up all

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var port = Number(process.env.PORT || 3000);
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello Node.js\n');
-}).listen(port);
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js\n');
+  }).listen(port);
 console.log('Server running');
 
 
