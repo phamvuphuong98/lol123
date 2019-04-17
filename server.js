752c50901cc58a2ce9edf3834e69927b124d667b
Phạm Vũ Phương <phamvuphuong98@gmail.com>
2019-04-17 11:16:24 +0700

    heroku

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ http.createServer(function (req, res) {
 console.log('Server running');
 
 
-const io = require('socket.io')(server);
+//const io = require('socket.io')(server);
 
-io.on('connection', function(socket) {
-    console.log(socket.id)
-    socket.on('SEND_NOTIFI', function(data) {
-        io.emit('NOTIFI', data)
-    });
-    socket.on('CREATE_JOB', function(data) {
-      io.emit('NOTIFI_CREATE_JOB', data)
-    });
-});
+//io.on('connection', function(socket) {
+//    console.log(socket.id)
+//    socket.on('SEND_NOTIFI', function(data) {
+//        io.emit('NOTIFI', data)
+//    });
+//    socket.on('CREATE_JOB', function(data) {
+//      io.emit('NOTIFI_CREATE_JOB', data)
+//    });
+//});
