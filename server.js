deb44f0acc7dd41964136391c4b60c70a4f28224
Phạm Vũ Phương <phamvuphuong98@gmail.com>
2019-04-17 11:07:21 +0700

    phuong

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-
-const express   = require('express'),
-      // http      = require('https'),
-      http      = require('http'),
-      fs 		= require('fs'),
-      app       = express()
-// var options = { 
-// 	key: fs.readFileSync('/etc/letsencrypt/live/devwork.vn/privkey.pem'),
-// 	cert: fs.readFileSync('/etc/letsencrypt/live/devwork.vn/cert.pem'),
-// 	ca: fs.readFileSync('/etc/letsencrypt/live/devwork.vn/chain.pem')
-// };
-// var service = http.createServer(options,app);
-var service = http.createServer(app);
-const server = service.listen(6001, function() {
-    console.log('server running on port 6001');
-});
+var http = require('http');
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello Node.js\n');
+}).listen(3000, "127.0.0.1");
+console.log('Server running');
 
 
 const io = require('socket.io')(server);
